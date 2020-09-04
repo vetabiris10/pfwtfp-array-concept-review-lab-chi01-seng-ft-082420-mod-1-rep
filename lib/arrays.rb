23239136f3884743db3b1775abf02c8d07d2b556
MARIUS MOLDOVAN <biris_21@yahoo.com>
2020-09-04 22:09:06 +0000

    Done.

--- lib/arrays.rb
+++ lib/arrays.rb
@@ -1,12 +1,16 @@
 #Write your code here!
 def using_max(my_array)
+  my_array.max
 end
 
 def find_nth_element(my_array)
+  my_array.find(Nemo)
 end
 
 def flatten_nested_arrays(my_array)
+  my_array.flatten
 end
 
 def flatten_then_sort(my_array)
+  my_array.flatten.sort
 end
